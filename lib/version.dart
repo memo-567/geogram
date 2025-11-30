8804ded7a2ad73816841fc86a2d99d92f067262f
Max Brito <maxbrito@pm.me>
2025-12-01 00:10:15 +0100

    Fix workflow permissions for release uploads

--- lib/version.dart
+++ lib/version.dart
@@ -1,4 +1,4 @@
 /// App version - keep in sync with pubspec.yaml
-const String appVersion = '1.4.0';
+const String appVersion = '1.4.1';
 const String appBuildNumber = '1';
 const String appFullVersion = '$appVersion+$appBuildNumber';
