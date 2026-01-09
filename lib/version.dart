8f4813b3888da1220cfbb1fef65ae1badaca8702
Max Brito <maxbrito@pm.me>
2026-01-09 08:33:28 +0100

    Release: bump version to 1.6.108+11

--- lib/version.dart
+++ lib/version.dart
@@ -1,6 +1,6 @@
 /// App version - auto-generated from pubspec.yaml
 /// Run: dart run tool/update_version.dart
 /// This file is automatically updated by the pre-commit hook
-const String appVersion = '1.6.107';
-const String appBuildNumber = '10';
+const String appVersion = '1.6.108';
+const String appBuildNumber = '11';
 const String appFullVersion = '$appVersion+$appBuildNumber';
