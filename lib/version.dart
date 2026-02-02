aeed792d65c4c89392ee4d924afcdc4d31cf7079
Max Brito <maxbrito@pm.me>
2026-02-02 17:51:37 +0100

    Bump version to 1.15.1

--- lib/version.dart
+++ lib/version.dart
@@ -1,6 +1,6 @@
 /// App version - auto-generated from pubspec.yaml
 /// Run: dart run tool/update_version.dart
 /// This file is automatically updated by the pre-commit hook
-const String appVersion = '1.15.0';
+const String appVersion = '1.15.1';
 const String appBuildNumber = '1';
 const String appFullVersion = '$appVersion+$appBuildNumber';
